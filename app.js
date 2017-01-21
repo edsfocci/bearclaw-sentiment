9911e2e97eb07b15a46631bb242d23833c34c1b2
Ed Solis <Ed@ALGLO.com>
2017-01-21 17:57:59 -0600

    Intentations were bugging me!

--- app.js
+++ app.js
@@ -21,24 +21,24 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 
 function rawBody(req, res, next) {
-    var buffers = [];
-    req.on("data", function(chunk) {
-        buffers.push(chunk);
-    });
-    req.on("end", function() {
-        req.rawBody = Buffer.concat(buffers);
-        next();
-    });
+  var buffers = [];
+  req.on("data", function(chunk) {
+    buffers.push(chunk);
+  });
+  req.on("end", function() {
+    req.rawBody = Buffer.concat(buffers);
+    next();
+  });
 }
 
 function errorHandler(err, req, res, next) {
-    if (res.headersSent) {
-        return next(err);
-    }
-    res.status(500);
-    res.render("error", {
-        error: err
-    });
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render("error", {
+    error: err
+  });
 }
 
 app.use(rawBody);
@@ -52,23 +52,23 @@ app.post("/webhook_callback", function(req, res) {
 
 
   if (!APP_ID || !APP_SECRET || !WEBHOOK_SECRET) {
-  	console.log("ERROR: Missing variables APP_ID, APP_SECRET or WEBHOOK_SECRET from environment");
-  	return;
+    console.log("ERROR: Missing variables APP_ID, APP_SECRET or WEBHOOK_SECRET from environment");
+    return;
   }
 
   if (!verifySender(req.headers, req.rawBody)) {
-      console.log("ERROR: Cannot verify caller! -------------");
-      console.log(req.rawBody.toString());
-      res.status(200).end();
-      return;
+    console.log("ERROR: Cannot verify caller! -------------");
+    console.log(req.rawBody.toString());
+    res.status(200).end();
+    return;
   }
 
   var body = JSON.parse(req.rawBody.toString());
   var eventType = body.type;
   if (eventType === "verification") {
-      handleVerificationRequest(res, body.challenge);
-      console.log("INFO: Verification request processed");
-      return;
+    handleVerificationRequest(res, body.challenge);
+    console.log("INFO: Verification request processed");
+    return;
   }
 
   // Acknowledge we received and processed notification to avoid getting sent the same event again
@@ -125,8 +125,8 @@ app.post("/webhook_callback", function(req, res) {
       return;
     }
   } else {
-      // Skip analysis we are not interested in
-      return;
+    // Skip analysis we are not interested in
+    return;
   }
 
   // Build request options for authentication.
@@ -134,11 +134,11 @@ app.post("/webhook_callback", function(req, res) {
     "method": "POST",
     "url": `${WWS_URL}${AUTHORIZATION_API}`,
     "auth": {
-        "user": APP_ID,
-        "pass": APP_SECRET
+      "user": APP_ID,
+      "pass": APP_SECRET
     },
     "form": {
-        "grant_type": "client_credentials"
+      "grant_type": "client_credentials"
     }
   };
 
@@ -146,21 +146,21 @@ app.post("/webhook_callback", function(req, res) {
 
     // If successful authentication, a 200 response code is returned
     if (response.statusCode !== 200) {
-        // if our app can't authenticate then it must have been disabled.  Just return
-        console.log("ERROR: App can't authenticate");
-        return;
+      // if our app can't authenticate then it must have been disabled.  Just return
+      console.log("ERROR: App can't authenticate");
+      return;
     }
     const accessToken = JSON.parse(authenticationBody).access_token;
 
     const GraphQLOptions = {
-        "url": `${WWS_URL}/graphql`,
-        "headers": {
-            "Content-Type": "application/graphql",
-            "x-graphql-view": "PUBLIC",
-            "jwt": "${jwt}"
-        },
-        "method": "POST",
-        "body": ""
+      "url": `${WWS_URL}/graphql`,
+      "headers": {
+        "Content-Type": "application/graphql",
+        "x-graphql-view": "PUBLIC",
+        "jwt": "${jwt}"
+      },
+      "method": "POST",
+      "body": ""
     };
 
     GraphQLOptions.headers.jwt = accessToken;
@@ -169,43 +169,43 @@ app.post("/webhook_callback", function(req, res) {
     request(GraphQLOptions, function(err, response, graphqlbody) {
 
       if (!err && response.statusCode === 200) {
-          const bodyParsed = JSON.parse(graphqlbody);
-          var person = bodyParsed.data.message.createdBy;
-					memberId = person.id;
-          memberName = person.displayName;
-          if (docSentiment.score > 0.50)
-              msgText = memberName + msgText;
-          else
-              msgText = 'Why did the chicken cross the road?' + msgText;
+        const bodyParsed = JSON.parse(graphqlbody);
+        var person = bodyParsed.data.message.createdBy;
+        memberId = person.id;
+        memberName = person.displayName;
+        if (docSentiment.score > 0.50)
+          msgText = memberName + msgText;
+        else
+          msgText = 'Why did the chicken cross the road?' + msgText;
 
       } else {
-          console.log("ERROR: Can't retrieve " + GraphQLOptions.body + " status:" + response.statusCode);
-          return;
+        console.log("ERROR: Can't retrieve " + GraphQLOptions.body + " status:" + response.statusCode);
+        return;
       }
 
       // Avoid endless loop of analysis :-)
-			if (memberId !== APP_ID) {
+      if (memberId !== APP_ID) {
         const appMessage = {
-            "type": "appMessage",
+          "type": "appMessage",
+          "version": "1",
+          "annotations": [{
+            "type": "generic",
             "version": "1",
-            "annotations": [{
-                "type": "generic",
-                "version": "1",
 
-                "title": "",
-                "text": "",
-                "color": "#ececec",
-            }]
+            "title": "",
+            "text": "",
+            "color": "#ececec",
+          }]
         };
 
         const sendMessageOptions = {
-            "url": "https://api.watsonwork.ibm.com/v1/spaces/${space_id}/messages",
-            "headers": {
-                "Content-Type": "application/json",
-                "jwt": ""
-            },
-            "method": "POST",
-            "body": ""
+          "url": "https://api.watsonwork.ibm.com/v1/spaces/${space_id}/messages",
+          "headers": {
+            "Content-Type": "application/json",
+            "jwt": ""
+          },
+          "method": "POST",
+          "body": ""
         };
 
         sendMessageOptions.url = sendMessageOptions.url.replace("${space_id}", spaceId);
@@ -217,7 +217,7 @@ app.post("/webhook_callback", function(req, res) {
         request(sendMessageOptions, function(err, response, sendMessageBody) {
 
           if (err || response.statusCode !== 201) {
-              console.log("ERROR: Posting to " + sendMessageOptions.url + "resulted on http status code: " + response.statusCode + " and error " + err);
+            console.log("ERROR: Posting to " + sendMessageOptions.url + "resulted on http status code: " + response.statusCode + " and error " + err);
           }
 
         });
@@ -232,36 +232,36 @@ app.post("/webhook_callback", function(req, res) {
 
 
 function verifySender(headers, rawbody) {
-    var headerToken = headers[WEBHOOK_VERIFICATION_TOKEN_HEADER];
-    var endpointSecret = WEBHOOK_SECRET;
-    var expectedToken = crypto
-        .createHmac("sha256", endpointSecret)
-        .update(rawbody)
-        .digest("hex");
+  var headerToken = headers[WEBHOOK_VERIFICATION_TOKEN_HEADER];
+  var endpointSecret = WEBHOOK_SECRET;
+  var expectedToken = crypto
+    .createHmac("sha256", endpointSecret)
+    .update(rawbody)
+    .digest("hex");
 
-    if (expectedToken === headerToken) {
-        return Boolean(true);
-    } else {
-        return Boolean(false);
-    }
+  if (expectedToken === headerToken) {
+    return Boolean(true);
+  } else {
+    return Boolean(false);
+  }
 }
 
 function handleVerificationRequest(response, challenge) {
-    var responseBodyObject = {
-        "response": challenge
-    };
-    var responseBodyString = JSON.stringify(responseBodyObject);
-    var endpointSecret = WEBHOOK_SECRET;
+  var responseBodyObject = {
+    "response": challenge
+  };
+  var responseBodyString = JSON.stringify(responseBodyObject);
+  var endpointSecret = WEBHOOK_SECRET;
 
-    var responseToken = crypto
-        .createHmac("sha256", endpointSecret)
-        .update(responseBodyString)
-        .digest("hex");
+  var responseToken = crypto
+    .createHmac("sha256", endpointSecret)
+    .update(responseBodyString)
+    .digest("hex");
 
-    response.writeHead(200, {
-        "Content-Type": "application/json; charset=utf-8",
-        "X-OUTBOUND-TOKEN": responseToken
-    });
+  response.writeHead(200, {
+    "Content-Type": "application/json; charset=utf-8",
+    "X-OUTBOUND-TOKEN": responseToken
+  });
 
-    response.end(responseBodyString);
+  response.end(responseBodyString);
 }
